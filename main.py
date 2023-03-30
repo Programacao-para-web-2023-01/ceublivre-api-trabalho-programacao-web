0349a8fa7045f48295d0fc602b04f9b04bf587d4
Matheus Santos Alencar <matheus.alencar@dinfor.uniceub>
2023-03-30 20:10:51 -0300

    add db connection

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
 
+conn = psycopg2.connect("postgresql://username:<ENTER-SQL-USER-PASSWORD>@db-app-693.g8x.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full")
+
 @app.get("/")
 async def root():
+    with conn.cursor() as cur:
+        cur.execute("SELECT now()")
+        res = cur.fetchall()
+        conn.commit()
+        print(res)
     return {"message": "Hello World"}
